2adfb82c27701135be5808666964bc3c664013fe
gvs89 <112775497+gvs89@users.noreply.github.com>
2023-02-02 21:48:47 +0300

    Update and rename website/settings.py to Веб-сайт/settings.py

--- Веб-сайт/settings.py
+++ Веб-сайт/settings.py
@@ -74,8 +74,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_orm_migrations',
-        'USER': 'flores',
-        'PASSWORD': 'zxczxc',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
